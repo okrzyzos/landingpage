da1c912f778daa13b0197a53cb984664e117b5f4
okrzyzos <okrzyzos@outlook.fr>
2021-10-26 18:17:41 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router} from 'react-router-dom'
+import Hero from './components/Hero';
+import Cakes from './components/Cakes';
+import Favorite from './components/Favorite';
+import Footer from './components/Footer';
+import Chefs from './components/Chefs';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+
+<div className="">
+<Hero />
+<Cakes id="menu"/>
+<Favorite />
+<Chefs  id="staff"/>
+<Footer id="contact" />
     </div>
+    </Router>
+   
   );
 }
 
